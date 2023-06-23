cf9152a3f6b6302859ce89a1531ca40efdd8142f
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-23 13:57:32 +0600

    83-3: Folder structure, import alias, page, layout, metadata and styling

---
+++ explore-next-js/src/app/page.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Home Page</h1>
+      <footer>Footer</footer>
+    </main>
+  );
+};
+
+export default HomePage;
