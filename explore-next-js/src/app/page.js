60eecc088a5bac5f91339905cd53346a27f9bff1
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-24 23:23:53 +0600

    added navbar for testing purpose

--- explore-next-js/src/app/page.js
+++ explore-next-js/src/app/page.js
@@ -1,6 +1,13 @@
 const HomePage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Dashboard</li>
+        </ul>
+      </nav>
       <h1>Home Page</h1>
       <footer>Footer</footer>
     </main>
