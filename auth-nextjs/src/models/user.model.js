d3b6c904799639e2356251902e441f53a1706a83
mahmud035 <mahamudulhasan4148@gmail.com>
2023-07-01 23:12:41 +0600

    create user model

---
+++ auth-nextjs/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+export const User = mongoose.models.users || mongoose.model('User', UserSchema);
