1a3f0e58273eaa187764fc7f95af111c05f54623
mahmud035 <mahamudulhasan4148@gmail.com>
2023-07-06 18:14:20 +0600

    Implement logout functionality

--- auth-nextjs/src/app/profile/page.tsx
+++ auth-nextjs/src/app/profile/page.tsx
@@ -1,9 +1,31 @@
+'use client';
+
 import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 const ProfilePage = () => {
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get('/api/users/logout');
+      console.log(response.data);
+      router.push('/login');
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Profile Page</h1>
+      <h1 className="text-3xl">Profile Page</h1>
+
+      <button
+        onClick={handleLogout}
+        className="p-2  border border-red-300 rounded-lg mt-4 focus:outline-none focus:border-red-600"
+      >
+        Logout
+      </button>
     </div>
   );
 };
