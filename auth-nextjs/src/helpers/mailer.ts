58a348b327aef8b5dc9f62579d76fa9b2f0cc3b9
mahmud035 <mahamudulhasan4148@gmail.com>
2023-07-07 20:39:04 +0600

    IMPORTANT NOTE about which route pattern should I follow

---
+++ auth-nextjs/src/helpers/mailer.ts
@@ -0,0 +1,6 @@
+// IMPORTANT: Which API route pattern should I follow?
+
+// It's depends. If I want to verify the user from Back-End, then I need to follow the first route pattern. Contrary, if I want to verify the user from Front-End, then I need to follow the second route pattern.
+
+// 1. domain.com/verifyToken/adasdasiafk (BACK-END VERIFY)
+// 2. domain.com/verifyToken?token=adasdasiafk (FRONT-END VERIFY)
